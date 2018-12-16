60265fd0d895e1404b05e14b41aa75216ef90a1c
dimakarts <kartsianovich@gmail.com>
2018-12-16 12:04:07 +0100

    added module publications

---
+++ common/modules/publications/Pages.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace common\modules\publications;
+
+/**
+ * publications module definition class
+ */
+class Pages extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'common\modules\publications\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
